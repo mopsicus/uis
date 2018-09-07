2d613095ff300d6fc0b64c3813e41251d876a45f
mopsicus <immops@gmail.com>
2018-09-07 13:59:14 +0300

    * Custom items height support added! * Callbacks refactoring * New demos added

---
+++ Demo/Assets/Scripts/Demo1.cs
@@ -0,0 +1,33 @@
+﻿using Mopsicus.InfiniteScroll;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Demo1 : MonoBehaviour {
+
+	[SerializeField]
+	private InfiniteScroll Scroll;
+
+	[SerializeField]
+	private int Count = 100;
+
+	void Start () {
+		Scroll.OnFill += OnFillItem;
+		Scroll.OnHeight += OnHeightItem;
+
+		Scroll.InitData (Count);
+	}
+
+	void OnFillItem (int index, GameObject item) {
+		item.GetComponentInChildren<Text> ().text = index.ToString ();
+	}
+
+	int OnHeightItem (int index) {
+		return 150;
+	}
+
+	public void SceneLoad (int index) {
+		SceneManager.LoadScene (index);
+	}
+
+}
--- Demo/readme.txt
+++
@@ -1,9 +0,0 @@
-Just put InfiniteScroll.cs with your simple ScrollView UI and write callbacks.
-It's easy to use, and fastes, no laggy, no jitter. Tested on iOS and Android.
-
-See demo and use.
-
-Fell free to contact me for help, support and examples:
-
-email: mail@mopsicus.ru
-website: mopsicus.ru
